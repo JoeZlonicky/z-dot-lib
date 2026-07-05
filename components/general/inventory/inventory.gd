84c72432bc7ee3597c69825a377a5754143b5e6b
Joe Zlonicky <joe.zlonicky@gmail.com>
2026-07-05 13:37:21 -0700

    Make inventory just use resource type

--- components/general/inventory/inventory.gd
+++ components/general/inventory/inventory.gd
@@ -2,23 +2,23 @@ class_name Inventory
 extends RefCounted
 
 
-signal item_added(item_data: ItemData, n: int)
-signal item_removed(item_data: ItemData, n: int)
+signal item_added(item_data: Resource, n: int)
+signal item_removed(item_data: Resource, n: int)
 signal changed
 
-var _items: Dictionary[ItemData, int] = {}
+var _items: Dictionary[Resource, int] = {}
 
 
-## Add a given number of [ItemData].
-func add_item(item_data: ItemData, count: int = 1) -> void:
+## Add a given number of an item.
+func add_item(item_data: Resource, count: int = 1) -> void:
 	assert(count >= 1)
 	_items[item_data] = _items.get(item_data, 0) + count
 	item_added.emit(item_data, count)
 	changed.emit()
 
 
-## Removes up to a given number of [ItemData].
-func remove_item(item_data: ItemData, count: int = 1) -> void:
+## Removes up to a given number of an item.
+func remove_item(item_data: Resource, count: int = 1) -> void:
 	assert(count >= 1)
 	
 	var current_count: int = _items.get(item_data, 0)
@@ -34,17 +34,17 @@ func remove_item(item_data: ItemData, count: int = 1) -> void:
 	changed.emit()
 
 
-## Gets all [ItemData] that are contained.
+## Gets all items that are contained.
 func get_items() -> Array:
 	return _items.keys()
 
 
 ## Returns the quantity of an item (0 if there are none).
-func get_item_count(item_data: ItemData) -> int:
+func get_item_count(item_data: Resource) -> int:
 	return _items.get(item_data, 0)
 
 
-func has_item(item_data: ItemData, quantity: int = 1) -> bool:
+func has_item(item_data: Resource, quantity: int = 1) -> bool:
 	assert(quantity >= 0)
 	return get_item_count(item_data) >= quantity
 
